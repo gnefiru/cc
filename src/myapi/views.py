00c40ce888f8f838009a842400578ac37c60cc77
Letitia <bodnar.letitia@gmail.com>
2020-04-06 12:21:28 +0000

    Add the test.txt file

---
+++ src/myapi/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .serializers import CharacterSerializer
+from .models import Character
+class CharacterViewSet(viewsets.ModelViewSet):
+	queryset = Character.objects.all().order_by('name')
+	serializer_class = CharacterSerializer
+# Create your views here.
